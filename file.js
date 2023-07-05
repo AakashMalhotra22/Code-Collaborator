b5ff74f0f79d0bf6e01f1d9a666d4d41cf163b1e
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-07-05 15:06:16 +0530

    setting http server and socket

---
+++ file.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+// console.log(__dirname);
+// const abs = path.join(__dirname,"server.js");
+console.log(path.resolve(""));
+
+const abs = path.join(__dirname,"index.html");
+console.log(abs);
--- server.js
+++ server.js
@@ -2,15 +2,26 @@ require('dotenv').config();
 
 const connectDB = require('./db/connect');
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
+const io = require('socket.io')(server);
 
-app.get('/',(req,res)=>{
-    res.send("hello");
-    return;
+io.on('connection',(socket)=>
+{
+    console.log("a user connected");
+    socket.on('disconnect',()=>
+    {
+        console.log("a user disconnected");
+    })
 })
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+})
 
 const start = async ()=>
 {
@@ -18,7 +29,7 @@ const start = async ()=>
     {
         await connectDB(process.env.MONGO_URL);
         console.log("MONGODB is connected");
-        app.listen(process.env.PORT,()=>
+        server.listen(process.env.PORT,()=>
         {
         console.log(`server is listening at port ${process.env.PORT}`);
         })
