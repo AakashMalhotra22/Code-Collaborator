de79a70989384549eb87be24c3aa69595e20b8d9
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-07-06 15:19:13 +0530

    created front page

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector("#codeContainer").style.display = "none";
 document.querySelector("#createFolder").style.display = "none";
 document.querySelector("#saveRoom").style.display = "none";
+
+const btn = document.getElementById("submit");
+btn.addEventListener("click",(e)=>
+{
+    e.preventDefault();
+    let roomName = document.getElementById('RoomName');
+    if(roomName.value =="")
+    {
+        alert("Enter a valid room Name");
+    }
+    
+})
