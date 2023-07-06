78f34fc1e1662f4b0c0525825668b2d79de72f44
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-07-06 15:44:19 +0530

    main page updation

--- script.js
+++ script.js
@@ -2,14 +2,34 @@ document.querySelector("#codeContainer").style.display = "none";
 document.querySelector("#createFolder").style.display = "none";
 document.querySelector("#saveRoom").style.display = "none";
 
-const btn = document.getElementById("submit");
-btn.addEventListener("click",(e)=>
+let roomName = document.getElementById('RoomName');
+roomName.addEventListener('keypress', (event)=>
 {
-    e.preventDefault();
-    let roomName = document.getElementById('RoomName');
+    if(event.key=='Enter')
+    {
+        event.preventDefault();
+        SubmitRoomId(); 
+    }
+});
+
+const joinButton = document.getElementById("join");
+joinButton.addEventListener("click",(event)=>
+{
+    event.preventDefault();
+    SubmitRoomId(); 
+});
+
+function SubmitRoomId()
+{
     if(roomName.value =="")
     {
-        alert("Enter a valid room Name");
+        alert("Enter a valid Room Id");
     }
-    
-})
+    else
+    {
+        document.querySelector("#codeContainer").style.display = "flex";
+        document.querySelector("#createFolder").style.display = "block";
+        document.querySelector("#saveRoom").style.display = "block";
+        document.querySelector(".lobby").style.display = "none";
+    }
+}
